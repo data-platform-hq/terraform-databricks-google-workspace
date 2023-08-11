0cd506f5fa8b836c290fd5a8741ab56a95293ffb
Ruslan Riznyk <ruslan_riznyk@epam.com>
2023-08-11 12:32:36 +0300

    chore: fixed TFlint warnings "${var.name}" => var.name commented unused data and variable added version constrain fo google provider 	modified:   main.tf 	modified:   variables.tf 	modified:   versions.tf

--- variables.tf
+++ variables.tf
@@ -26,11 +26,11 @@ variable "region" {
   default     = "us-west1"
 }
 
-variable "databricks_google_service_account" {
-  description = "value"
-  type        = string
-  default     = null
-}
+# variable "databricks_google_service_account" {
+#   description = "value"
+#   type        = string
+#   default     = null
+# }
 
 variable "databricks_vpc" {
   description = "value"
--- versions.tf
+++ versions.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.69.1"
+    }
     databricks = {
       source  = "databricks/databricks"
       version = ">= 1.19.0"
